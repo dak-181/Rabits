d578b26011a44252ad606be7684e0f72cdc941f9
dak-181 <austinbartlett.asb@gmail.com>
2026-03-04 22:04:27 -0700

    Updated to include a graph

--- main.py
+++ main.py
@@ -2,29 +2,42 @@
 
 import os
 import sys
-import matplotlib
 import math
+import matplotlib.pyplot as plt
+import numpy as np
 
 
+# TODO: use the formula in the readme video
 # take a starting population and double it x times
-def calculateNewPop(startPop, cycles):
+def calculateNewPopArrays(startPop, cycles):
 
     counter = cycles
-    newPop = startPop
 
-    while counter > 0:
-        newPop = newPop * 2
+    newPopArray = []
+    newPopArray.append(startPop)
+    cycleArray = []
+    cycleArray.insert(0, cycles + 1)
+
+    while counter > 0 :
+
+        newPopArray.append(newPopArray[-1] * 2)
+        cycleArray.insert(0, counter)
+
         counter = counter - 1
 
-    return newPop
+    return newPopArray, cycleArray
 
 
 # input but requires an integer
 def getIntegerInput(prompt_message):
+
     while True:
+
         user_input = input(prompt_message)
+
         try:
             return int(user_input)
+        
         except ValueError:
             print("Invalid input. Please enter a whole number.")
 
@@ -35,9 +48,19 @@ def main():
     startPop = getIntegerInput("How many rabbits?\n")
     cycles = getIntegerInput("How much growth?\n")
 
-    newPop = calculateNewPop(startPop, cycles)
+    newPopArray , cycleArray = calculateNewPopArrays(startPop, cycles)
 
-    print("New rabbit population: ", newPop)
+    print("New rabbit population: ", newPopArray[-1])
+    #print(cycleArray, newPopArray)
+
+    plt.plot(cycleArray, newPopArray)
+    plt.title("Rabbit Population Over Time")
+    plt.xlabel("Years")
+    plt.ylabel("Number of Rabbits")
+    plt.grid(True)
+
+    plt.show()
+
 
 if __name__ == "__main__":
     main()
