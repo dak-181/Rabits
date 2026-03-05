e50e02b71c4ef82be54207d9b6ab6cd427b75546
dak-181 <austinbartlett.asb@gmail.com>
2026-03-04 21:26:34 -0700

    First real program

--- main.py
+++ main.py
@@ -3,5 +3,41 @@
 import os
 import sys
 import matplotlib
+import math
 
-print("You did it!")
+
+# take a starting population and double it x times
+def calculateNewPop(startPop, cycles):
+
+    counter = cycles
+    newPop = startPop
+
+    while counter > 0:
+        newPop = newPop * 2
+        counter = counter - 1
+
+    return newPop
+
+
+# input but requires an integer
+def getIntegerInput(prompt_message):
+    while True:
+        user_input = input(prompt_message)
+        try:
+            return int(user_input)
+        except ValueError:
+            print("Invalid input. Please enter a whole number.")
+
+
+# core functions
+def main():
+
+    startPop = getIntegerInput("How many rabbits?\n")
+    cycles = getIntegerInput("How much growth?\n")
+
+    newPop = calculateNewPop(startPop, cycles)
+
+    print("New rabbit population: ", newPop)
+
+if __name__ == "__main__":
+    main()
